dd8332414f00495493b5084b6f04fd60dc2533f4
Rasmus Kriest <rasmusk@outlook.com>
2017-09-26 23:50:00 +0200

    Add Google Drive File Stream

---
+++ google-drive-file-stream/tools/chocolateyinstall.ps1
@@ -0,0 +1,38 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= $env:ChocolateyPackageName
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'google-drive-file-stream*'
+  checksum      = 'c015c613d5ba54941934df3045459091'
+  checksumType  = 'md5'
+  silentArgs   = '--silent'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
