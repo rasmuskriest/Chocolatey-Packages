5868393709dbb3a53891d5ad912dea8ab8bf8ff6
Rasmus Kriest <rasmusk@outlook.com>
2017-09-27 00:47:44 +0200

    Include AutoHotKey file

--- BingDesktop/tools/chocolateyInstall.ps1
+++ BingDesktop/tools/chocolateyInstall.ps1
@@ -3,6 +3,10 @@
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://download.microsoft.com/download/0/7/8/078E99E2-F1AE-4F46-B3EE-8208B7D01E5C/BDT5/BingDesktopSetup.exe'
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = Join-Path $toolsDir "GBASInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
 
 $packageArgs = @{
   packageName   = $packageName
