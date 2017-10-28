f5cd4a9773ad07ce968e411968718e8e9be9405e
Rasmus Kriest <rasmusk@outlook.com>
2017-10-29 01:40:06 +0200

    Fix accidentally included AutoHotKey

--- google-drive-file-stream/tools/chocolateyinstall.ps1
+++ google-drive-file-stream/tools/chocolateyinstall.ps1
@@ -3,18 +3,14 @@
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe'
-$ahkExe         = 'AutoHotKey'
-$ahkFile        = Join-Path $toolsDir "bingdesktopinstall.ahk"
-
-Start-Process $ahkExe $ahkFile
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-  softwareName  = 'google-drive-file-stream*'
-  checksum      = '9c29caaf38c41c7e46b0855024943b9b'
+  softwareName  = 'Google Drive File Stream*'
+  checksum      = '436d0c813f602c7e9e15bfe68c302ead'
   checksumType  = 'md5'
   silentArgs   = '--silent'
   validExitCodes= @(0)
