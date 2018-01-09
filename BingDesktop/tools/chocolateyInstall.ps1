7635075156637c071d3fd3e987f22ae1b563b947
Rasmus Kriest <rasmusk@outlook.com>
2018-01-09 18:37:51 +0100

    Format code using Prettier

--- BingDesktop/tools/chocolateyInstall.ps1
+++ BingDesktop/tools/chocolateyInstall.ps1
@@ -1,23 +1,23 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= $env:ChocolateyPackageName
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://download.microsoft.com/download/0/7/8/078E99E2-F1AE-4F46-B3EE-8208B7D01E5C/BDT5/BingDesktopSetup.exe'
-$ahkExe         = 'AutoHotKey'
-$ahkFile        = Join-Path $toolsDir "bingdesktopinstall.ahk"
+$packageName = $env:ChocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://download.microsoft.com/download/0/7/8/078E99E2-F1AE-4F46-B3EE-8208B7D01E5C/BDT5/BingDesktopSetup.exe'
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $toolsDir "bingdesktopinstall.ahk"
 
 Start-Process $ahkExe $ahkFile
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  softwareName  = 'Bing*'
-  checksum      = 'c6fc8b635dfdf72cf69fd42899bc86f1'
-  checksumType  = 'md5'
-  silentArgs   = '/Q'
-  validExitCodes= @(0)
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    softwareName   = 'Bing*'
+    checksum       = 'c6fc8b635dfdf72cf69fd42899bc86f1'
+    checksumType   = 'md5'
+    silentArgs     = '/Q'
+    validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
--- google-drive-file-stream/tools/chocolateyinstall.ps1
+++ google-drive-file-stream/tools/chocolateyinstall.ps1
@@ -1,19 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= $env:ChocolateyPackageName
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe'
+$packageName = $env:ChocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  softwareName  = 'Google Drive File Stream*'
-  checksum      = 'ee9147dbaced32123c576f088e6de525'
-  checksumType  = 'md5'
-  silentArgs   = '--silent'
-  validExitCodes= @(0)
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    softwareName   = 'Google Drive File Stream*'
+    checksum       = 'ee9147dbaced32123c576f088e6de525'
+    checksumType   = 'md5'
+    silentArgs     = '--silent'
+    validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
