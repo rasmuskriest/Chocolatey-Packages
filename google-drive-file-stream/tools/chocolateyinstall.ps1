889b2c40dff3f7f27de09ebe543739fbdcc103ab
Rasmus Kriest <rasmusk@outlook.com>
2018-03-11 09:50:52 +0100

    Update Drive File Stream to 25.157.185.3

--- google-drive-file-stream/tools/chocolateyinstall.ps1
+++ google-drive-file-stream/tools/chocolateyinstall.ps1
@@ -1,19 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= $env:ChocolateyPackageName
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe'
+$packageName = $env:ChocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  softwareName  = 'Google Drive File Stream*'
-  checksum      = '0f4ca2cafcfc99f98de7f6e268c0d9db'
-  checksumType  = 'md5'
-  silentArgs   = '--silent'
-  validExitCodes= @(0)
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    softwareName   = 'Google Drive File Stream*'
+    checksum       = '0d4edcaacebb63c877c05dc5778ffaf5'
+    checksumType   = 'md5'
+    silentArgs     = '--silent'
+    validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
