aa0c4fb7a7b51e810d61427663b2b02974f2ed79
rasmuskriest <rasmusk@outlook.com>
2018-12-01 15:51:35 +0300

    Add Russian Grammatical Dictionary

---
+++ russian-grammatical-dictionary/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿
+$packageName = $env:ChocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://www.solarix.ru/for_developers/download/win/russian-grammar-setup.exe'
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $toolsDir "rgdinstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    softwareName   = 'Russian Grammatical Dictionary'
+    checksum       = '608D2C181EC084FC84E1DED3671C2063DDA7022B3913C2481B6C6472B5C0CE3B'
+    checksumType   = 'sha256'
+    silentArgs     = '/S'
+    validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
