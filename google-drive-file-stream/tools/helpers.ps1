5790866e2e4372a5139daa3a292c4d5da6913398
rasmuskriest <rasmusk@outlook.com>
2021-02-02 09:11:56 +0100

    Include check for x64 to prevent errors on x86 during installation

--- google-drive-file-stream/tools/helpers.ps1
+++ google-drive-file-stream/tools/helpers.ps1
@@ -6,10 +6,12 @@ function AlreadyInstalled() {
     $uninstallEntry = $(
         Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" -Recurse | Get-ItemProperty | Where-Object DisplayName -eq $product
     )
-    
-    $uninstallEntryWow64 = $(
-        Get-ChildItem "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\" -Recurse | Get-ItemProperty | Where-Object DisplayName -eq $product
-    )
+    if ([System.Environment]::Is64BitOperatingSystem -eq $TRUE) {
+        $uninstallEntryWow64 = $(
+            Get-ChildItem "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\" -Recurse | Get-ItemProperty | Where-Object DisplayName -eq $product
+        
+        )
+    }
   
     if ($uninstallEntry -or $uninstallEntryWow64) {
         return $true
