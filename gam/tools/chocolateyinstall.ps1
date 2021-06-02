b3d71be6f06b5f689f81a5d5a23b2da002d86f5f
rasmuskriest <rasmusk@outlook.com>
2021-06-02 09:00:45 +0200

    Manually update to gam to 6.04

--- gam/tools/chocolateyinstall.ps1
+++ gam/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/jay0lee/GAM/releases/download/v5.10/gam-5.10-windows-x86.msi'
-$url64 = 'https://github.com/jay0lee/GAM/releases/download/v5.10/gam-5.10-windows-x86_64.msi'
+$url = 'https://github.com/jay0lee/GAM/releases/download/v6.04/gam-6.04-windows-x86.msi'
+$url64 = 'https://github.com/jay0lee/GAM/releases/download/v6.04/gam-6.04-windows-x86_64.msi'
 
 $ahkExe = 'AutoHotKey'
 $ahkFile = Join-Path $toolsDir "gaminstall.ahk"
@@ -17,9 +17,9 @@ $packageArgs = @{
 
   softwareName   = 'gam*'
 
-  checksum       = 'f90f6cdec210c42d8a5e6a5dcba5e90cea62e88cc317c7b96a29d361e1265160'
+  checksum       = '7f93f6d1970fa48a06e319df2fccbea444eb1ebd6c29242c271b15a1fa141356'
   checksumType   = 'sha256'
-  checksum64     = '535795b1e26458d5ffa61f4eca5e111fb0fdac35936701cc6f8b95bbbe6f588c'
+  checksum64     = '03b045f206deb48ce359ff3460b9fb1c36f1782a72e6d8eb353851abfa3f1720'
   checksumType64 = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
