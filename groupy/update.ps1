106a32752181d9c4855f5f4603b0fddd9286574c
Alex Lopez <info@ithinkvirtual.com>
2022-08-08 15:45:33 -0400

    Update update.ps1

--- groupy/update.ps1
+++ groupy/update.ps1
@@ -13,20 +13,16 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $releases = Invoke-WebRequest -Uri $history_page
-
-    $tag = "h2"
-    $re = "\w[Groupy]\s\d"
-    $ver = "\d\.\d{0,}(\n|$|\.\d{0,})"
-
-    $version = (($releases.AllElements | Where-Object { $_.TagName -eq $tag -and $_.innerText -Match $re }).innerText | Select-Object -First 1) -Match $ver
+    
     $url = "https://cdn.stardock.us/downloads/public/software/groupy/Groupy_setup.exe"
-    # Old $url including the specific version. Keeping for now as Stardock changes their URL scheme often.
-    # $url = ( -Join ("https://cdn.stardock.us/downloads/public/software/groupy/Groupy", $Matches[0], "_setup_sd.exe") )
+
+    $re = "SoundPackager (?<version>[\d\.]+[\d\.]+)"
+    $version = $releases -match $re | ForEach-Object { $Matches.version }
 
     return @{
         URL32   = $url
-        Version = $Matches[0]
+        Version = $version
     }
 }
 
-Update-Package
+Update-Package -ChecksumFor none
