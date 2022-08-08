6e435fa49079c0c96adf59c1dccc91b7d3b542b2
Alex Lopez <info@ithinkvirtual.com>
2022-08-08 15:51:49 -0400

    Update chocolateyinstall.ps1

--- groupy/tools/chocolateyinstall.ps1
+++ groupy/tools/chocolateyinstall.ps1
@@ -1,25 +1,36 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = $env:ChocolateyPackageName
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://cdn.stardock.us/downloads/public/software/groupy/Groupy_setup.exe'
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://cdn.stardock.us/downloads/public/software/groupy/Groupy_setup.exe'
+$checksum               = 'd7a1bf094f00708522b6ac5d06ca0770dcf5eba696808de1d9dedb4feacb9aca'
+$checksumType           = 'sha256'
+
 $killscript = Join-Path $toolsDir "groupykillprocess.ps1"
 
 $killjob = (Start-Process powershell $killscript -PassThru).ID
 
 $packageArgs = @{
-    packageName    = $packageName
+    packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
     fileType       = 'exe'
-    url            = $url
     softwareName   = 'Stardock Groupy*'
-    checksum       = 'd7a1bf094f00708522b6ac5d06ca0770dcf5eba696808de1d9dedb4feacb9aca'
-    checksumType   = 'sha256'
-    silentArgs     = '/S'
+    url            = $url
+    checksum       = $checksum
+    checksumType   = $checksumType
+    silentArgs     = '/s'
     validExitCodes = @(0, 9)
 }
 
-Install-ChocolateyPackage @packageArgs
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.1.7601'
+
+If ( $OSVer -lt [version]$min ) {
+  Write-Warning "*** Stardock Groupy requires a desktop OS running Windows 7 or higher... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
 
 Start-Sleep 6
 if (Get-Process -Id $killjob -ErrorAction SilentlyContinue) {
